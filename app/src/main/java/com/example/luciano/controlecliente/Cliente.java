67c19b1897da31b45bc1abd526223adf0a8ee40d
Luciano Carvalho <lucianoalves413@gmail.com>
2016-11-07 17:30:45 -0300

    Criado as classes cliente e red

--- app/src/main/java/com/example/luciano/controlecliente/Cliente.java
+++ app/src/main/java/com/example/luciano/controlecliente/Cliente.java
@@ -6,5 +6,58 @@ import com.orm.SugarRecord;
  * Created by Luciano on 07/11/2016.
  */
 public class Cliente extends SugarRecord{
-    
+
+    private long _id;
+    private long Codigo;
+    private String Nome;
+    private String rua;
+    private String Cidade;
+
+    public Cliente() {
+    }
+
+    public long get_id() {
+        return _id;
+    }
+
+    public void set_id(long _id) {
+        this._id = _id;
+    }
+
+    public long getCodigo() {
+        return Codigo;
+    }
+
+    public void setCodigo(long codigo) {
+        Codigo = codigo;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getCidade() {
+        return Cidade;
+    }
+
+    public void setCidade(String cidade) {
+        Cidade = cidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Código:  "+this.Codigo+" Nome: "+this.Nome;
+    }
 }
--- app/src/main/java/com/example/luciano/controlecliente/Red.java
+++ app/src/main/java/com/example/luciano/controlecliente/Red.java
@@ -7,4 +7,25 @@ import com.orm.SugarRecord;
  */
 public class Red extends SugarRecord{
 
+    private long _id;
+    private double nota;
+
+    public Red() {
+    }
+
+    public long get_id() {
+        return _id;
+    }
+
+    public void set_id(long _id) {
+        this._id = _id;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
 }
